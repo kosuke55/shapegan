b4c3dd48f60869078a7319ef5c220224aad36764
Marian Kleineberg <mail@marian42.de>
2020-02-07 15:00:06 +0100

    Update the voxel dataset class for the brains dataset

--- datasets.py
+++ datasets.py
@@ -3,65 +3,60 @@ from torch.utils.data import Dataset
 import os
 import numpy as np
 
-class VoxelsSingleTensor(Dataset):
-    def __init__(self, filename, clamp=0.1):
-        self.data = torch.load(filename)
-        if clamp is not None:
-            self.data.clamp_(-clamp, clamp)
-
-    def __getitem__(self, index):
-        return self.data[index, :, :, :]
-
-    def __len__(self):
-        return self.data.shape[0]
-    
-    def show(self):
-        show_dataset(self)
-
-class VoxelsMultipleFiles(Dataset):
-    def __init__(self, files, clamp=0.1):
+class VoxelDataset(Dataset):
+    def __init__(self, files, clamp=10, rescale_sdf=True):
         self.files = files
         self.clamp = clamp
+        self.rescale_sdf = rescale_sdf
+        self.remove_nan = True
 
     def __len__(self):
         return len(self.files)
 
+    def get_file_name(self, index):
+        return os.path.basename(self.files[index])
+
     def __getitem__(self, index):
         array = np.load(self.files[index])
+
+        file_name = self.get_file_name(index)
+        if file_name.startswith('baboon'):
+            array = array.transpose([0, 2, 1])
+        elif file_name.startswith('human'):
+            array = np.array(array.transpose([0, 2, 1]))
+        else:
+            array = np.array(array.transpose([0, 2, 1])[::-1, ::-1, ::-1])
+
+        if self.remove_nan:
+            array[~np.isfinite(array)] = 0
+
         result = torch.from_numpy(array)
         if self.clamp is not None:
             result.clamp_(-self.clamp, self.clamp)
+            if self.rescale_sdf:
+                result /= self.clamp
         return result
 
     @staticmethod
-    def glob(directory, extension='-32.npy'):
+    def glob(pattern):
         import glob
-        files = glob.glob(directory, recursive=True)
-        return VoxelsMultipleFiles(files)
-    
-    @staticmethod
-    def from_split(pattern, split_file_name):
-        split_file = open(split_file_name, 'r')
-        ids = split_file.readlines()
-        files = [pattern.format(id.strip()) for id in ids]
-        files = [file for file in files if os.path.exists(file)]
-        return VoxelsMultipleFiles(files)
-    
+        files = glob.glob(pattern, recursive=True)
+        if len(files) == 0:
+            raise Exception(
+                'No files found for glob pattern {:s}.'.format(pattern))
+        return VoxelDataset(sorted(files))
+
     def show(self):
-        show_dataset(self)
+        from rendering import MeshRenderer
+        import time
+        from tqdm import tqdm
 
-def show_dataset(dataset):
-    from rendering import MeshRenderer
-    import time
-    from tqdm import tqdm
+        viewer = MeshRenderer()
+        for item in tqdm(self):
+            viewer.set_voxels(item.numpy())
+            time.sleep(0.5)
 
-    viewer = MeshRenderer()
-    for item in tqdm(dataset):
-        viewer.set_voxels(item.numpy())
-        time.sleep(0.5)
 
 if __name__ == '__main__':
-    dataset = VoxelsMultipleFiles.glob('data/sdf/**/*-128.npy')
-    print(len(dataset))
-    #dataset = VoxelsMultipleFiles.from_split('data/chairs/voxels_{:d}/{{:s}}.npy'.format(64), 'data/chairs/train.txt')
+    dataset = VoxelDataset.glob('data/sdf-volumes/**/*.npy')
     dataset.show()
